3ac8edd6aa3c2313b5f0794dc7f8f26da1fd78c2
AlbinDeploy <albinpremam12@gmail.com>
2026-03-17 21:10:42 +0700

    Update route.js

--- app/api/android/route.js
+++ app/api/android/route.js
@@ -1,1 +1,15 @@
+const androidHosts = `178.128.211.32 growtopia1.com
+178.128.211.32 growtopia2.com
+178.128.211.32 www.growtopia1.com
+178.128.211.32 www.growtopia2.com
+178.128.211.32 growtopiagame.com`;
+
+export async function GET() {
+  return new Response(androidHosts, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Content-Disposition': 'inline; filename="android.txt"'
+    }
+  });
+}
 
