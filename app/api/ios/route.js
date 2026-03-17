3bfcddbaecb7e60e23b0a69a7bca2573591bf939
AlbinDeploy <albinpremam12@gmail.com>
2026-03-17 21:11:05 +0700

    Update route.js

--- app/api/ios/route.js
+++ app/api/ios/route.js
@@ -1,1 +1,18 @@
+const iosHosts = `[General]
+
+[Rule]
+FINAL,DIRECT
+
+[Host]
+www.growtopia1.com = 178.128.211.32
+www.growtopia2.com = 178.128.211.32`;
+
+export async function GET() {
+  return new Response(iosHosts, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Content-Disposition': 'inline; filename="ios.txt"'
+    }
+  });
+}
 
