10309633a6b6870f90636feca698f743875cec02
AlbinDeploy <albinpremam12@gmail.com>
2026-03-25 21:44:09 +0700

    Update route.js

--- app/api/android/route.js
+++ app/api/android/route.js
@@ -1,8 +1,8 @@
-const androidHosts = `178.128.61.71 growtopia1.com
-178.128.61.71 growtopia2.com
-178.128.61.71 www.growtopia1.com
-178.128.61.71 www.growtopia2.com
-178.128.61.71 growtopiagame.com`;
+const androidHosts = `168.144.39.237 growtopia1.com
+168.144.39.237 growtopia2.com
+168.144.39.237 www.growtopia1.com
+168.144.39.237 www.growtopia2.com
+168.144.39.237 growtopiagame.com`;
 
 export async function GET() {
   return new Response(androidHosts, {
