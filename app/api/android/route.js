4ff5b26ce9f09ee713d79a143bc9572542aa0586
AlbinDeploy <albinpremam12@gmail.com>
2026-04-09 17:37:15 +0700

    Update route.js

--- app/api/android/route.js
+++ app/api/android/route.js
@@ -1,8 +1,8 @@
-const androidHosts = `139.99.220.212 growtopia1.com
-139.99.220.212 growtopia2.com
-139.99.220.212 www.growtopia1.com
-139.99.220.212 www.growtopia2.com
-139.99.220.212 growtopiagame.com`;
+const androidHosts = `165.232.172.148 growtopia1.com
+165.232.172.148 growtopia2.com
+165.232.172.148 www.growtopia1.com
+165.232.172.148 www.growtopia2.com
+165.232.172.148 growtopiagame.com`;
 
 export async function GET() {
   return new Response(androidHosts, {
